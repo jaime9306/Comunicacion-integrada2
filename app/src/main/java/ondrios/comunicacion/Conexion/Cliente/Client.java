81d640ef630faabd1aed36d2eb0c6feffb590f68
Juan <juanx1194@gmail.com>
2016-01-11 11:31:59 +0100

    Motor

--- app/src/main/java/ondrios/comunicacion/Conexion/Cliente/Client.java
+++ app/src/main/java/ondrios/comunicacion/Conexion/Cliente/Client.java
@@ -254,7 +254,7 @@ public class Client  {
                     }
                 } else {
                     ClientActivity ca = (ClientActivity) context;
-                    ca.reparteCartas(cartaRobaPinte);
+                    ca.desaparecePinte(cartaRobaPinte);
                     if (turnoRobaPinte.equals(id)) {
                         ca.notificaTurno();
                         ca.setTurno();
